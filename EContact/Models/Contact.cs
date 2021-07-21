b6b0de5a9cca405d7418b7163f987b3e84386315
stranging <stranging@free.fr>
2021-07-21 17:20:00 +0200

    Ajoutez des fichiers projet.

---
+++ EContact/Models/Contact.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EContact.Models
+  {
+  public class Contact
+    {
+    public int Id { get; set; }
+    public string NomComplet { get; set; }
+    public DateTime DateNaiss { get; set; }
+    public string Email { get; set; }
+    public string  Telephone { get; set; }
+    public string Genre { get; set; }
+    public string UrlPhoto { get; set; }
+
+    public Contact()
+      {
+      }
+
+    public Contact(string nomComplet, DateTime dateNaiss, string email, string telephone, string genre, string urlPhoto)
+      {      
+      NomComplet = nomComplet;
+      DateNaiss = dateNaiss;
+      Email = email;
+      Telephone = telephone;
+      Genre = genre;
+      UrlPhoto = urlPhoto;
+      }
+    }
+  }
